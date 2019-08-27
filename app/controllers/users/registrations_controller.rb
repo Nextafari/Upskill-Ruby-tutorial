e4db5b0f039fd49e7d10e19c897639a14262e4ff
Emmanuel Chinedu <chinedue16@gmail.com>
2019-08-27 18:49:11 +0000

    Updated flash message styles and added fade out in js

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+    # Extend default Devise gem behaviour so that
+    # users signing up with the Pro account (plan ID 2)
+    # Save with a special Stripe subscription function.
+    # Otherwise Devise signs up user as usual.
+    
    def create
      super do |resource|
        if params[:plan]
